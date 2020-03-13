8bb826e6ebced601bcb2aaf056a371b734ec8175
Salah Eddine Maarouf <s.maarouf@netopia.ma>
2020-03-13 16:13:11 +0100

    Commit1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCY8S_y4gvPWcTBrOTpbKU3Vg9ipGT-59A",
+    authDomain: "oshop-bd5e1.firebaseapp.com",
+    databaseURL: "https://oshop-bd5e1.firebaseio.com",
+    projectId: "oshop-bd5e1",
+    storageBucket: "oshop-bd5e1.appspot.com",
+    messagingSenderId: "779727524883",
+    appId: "1:779727524883:web:ad62f974c486a470211be6",
+    measurementId: "G-0VPLMF3EW7"
+  }
 };
